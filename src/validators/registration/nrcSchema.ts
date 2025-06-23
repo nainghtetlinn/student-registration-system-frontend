140bd98affed27311c420da53a5c8cbc382b9afe
nainghtetlinn <naing.work.it@gmail.com>
2025-06-23 21:13:02 +0630

    feat: enhance nrcSchema validation with regex and add corresponding tests

--- src/validators/registration/nrcSchema.ts
+++ src/validators/registration/nrcSchema.ts
@@ -4,5 +4,8 @@ export const nrcSchema = z.object({
   stateCode: z.string().min(1),
   townshipCode: z.string().min(1),
   nrcType: z.string().min(1),
-  nrcNumber: z.string().min(1),
+  nrcNumber: z
+    .string()
+    .trim()
+    .regex(/^[၀၁၂၃၄၅၆၇၈၉]{6}$/),
 })
---
+++ tests/validators/registration/nrcSchema.test.ts
@@ -0,0 +1,49 @@
+import { nrcSchema } from '@/validators/registration/nrcSchema'
+
+describe('nrcSchema validation', () => {
+  it('testValidNrcObject', () => {
+    const validData = {
+      stateCode: '၁၃',
+      townshipCode: 'တကန',
+      nrcType: 'နိုင်',
+      nrcNumber: '၁၂၃၄၅၆',
+    }
+    expect(nrcSchema.safeParse(validData).success).toBe(true)
+  })
+  it('testInvalidNrcObject', () => {
+    const invalidData = {
+      stateCode: '၁၃',
+      townshipCode: 'တကန',
+      nrcType: 'နိုင်',
+      nrcNumber: '123456',
+    }
+    expect(nrcSchema.safeParse(invalidData).success).toBe(false)
+  })
+  it('testIncalidNrcObject', () => {
+    const invalidData = {
+      stateCode: '၁၃',
+      townshipCode: 'တကန',
+      nrcType: 'နိုင်',
+      nrcNumber: '123၄၅၆',
+    }
+    expect(nrcSchema.safeParse(invalidData).success).toBe(false)
+  })
+  it('testInvalidNrcObject', () => {
+    const invalidData = {
+      stateCode: '၁၃',
+      townshipCode: 'တကန',
+      nrcType: 'နိုင်',
+      nrcNumber: '၁၂၃၄၅',
+    }
+    expect(nrcSchema.safeParse(invalidData).success).toBe(false)
+  })
+  it('testInvalidNrcObject', () => {
+    const invalidData = {
+      stateCode: '၁၃',
+      townshipCode: 'တကန',
+      nrcType: 'နိုင်',
+      nrcNumber: '12345',
+    }
+    expect(nrcSchema.safeParse(invalidData).success).toBe(false)
+  })
+})

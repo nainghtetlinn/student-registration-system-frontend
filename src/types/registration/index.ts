97d46fa9063a424bbc1dc04326381cf20dce0141
nainghtetlinn <naing.work.it@gmail.com>
2025-06-25 19:55:07 +0630

    feat: remove unused registration type definitions

--- src/types/registration/index.ts
+++
@@ -1,9 +0,0 @@
-import { studentDetailsFormSchema } from '@/validators/registration/studentDetailsFormSchema'
-import { formalStudentRegistrationFormSchema } from '@/validators/registration/formalStudentRegistrationFormSchema'
-
-import { z } from 'zod'
-
-export type StudentDetailsFormSchema = z.infer<typeof studentDetailsFormSchema>
-export type FormalStudentRegistrationFormSchema = z.infer<
-  typeof formalStudentRegistrationFormSchema
->

646433182c96a8bf3c935395da4a56b2e9f61ee4
nainghtetlinn <naing.work.it@gmail.com>
2025-06-28 05:51:10 +0630

    feat: add Toaster component in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { Toaster } from 'sonner'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { ReactQueryClientProvider } from '@/providers/query-provider'
 
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         <ThemeProvider
           attribute='class'
           defaultTheme='system'

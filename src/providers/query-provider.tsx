f6141464c854a25683c465109e46ad3f3b673b8a
nainghtetlinn <naing.work.it@gmail.com>
2025-07-05 10:21:18 +0630

    feat: add React Query Devtools for enhanced query debugging

--- src/providers/query-provider.tsx
+++ src/providers/query-provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,6 +16,9 @@ export const ReactQueryClientProvider = ({
   children: React.ReactNode
 }>) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
   )
 }
